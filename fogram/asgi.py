1ea214c790dccaa835cf6487f1132fefcce4210e
broten15 <brotensam@gmail.com>
2020-08-16 01:59:21 -0500

    django env setup

---
+++ fogram/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for fogram project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fogram.settings')
+
+application = get_asgi_application()
